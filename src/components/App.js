9da1f638a66f3e9d43d113ae5fe6a11ab9f5c9f3
notkyraxo <kcdiaz@uw.edu>
2024-05-23 13:59:53 -0700

    imported linking and routing libraries; created routes for each page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { MealPage } from './MealPage';
 import { AddMeal } from './AddMeal.js'; 
 import { HomePage } from './HomePage.js';
 import { FavoritesList } from './FavoritesList.js';
+import { Routes, Route, Navigate, Link } from 'react-router-dom'; 
 
 export function App(props) {
     return (
@@ -13,21 +14,26 @@ export function App(props) {
                 <a href="#"><i className="fa fa-bars" aria-label="menu"></i></a>
             </div>
             <div className="laptop-nav">
-                <a href="index.html"><span className="material-icons" aria-label="Home">home</span></a> 
+                <Link to="home"><span className="material-icons" aria-label="Home">home</span></Link> 
                 <button type="button" class="btn btn-light custom-color" aria-labelledby="favorites list">
-                    <a href="favorites-list.html">Favorites &#x2665;</a>
+                    <Link to="favorites">Favorites &#x2665;</Link>
                 </button>
                 <button type="button" class="btn btn-light custom-color" aria-labelledby="add meal form">
-                    <a href="add-meal.html">Add Meal</a>
+                    <Link to="addMeal">Add Meal</Link>
                 </button>
             </div>
         </nav>
 
-        {/* <HomePage /> */}
-        {/* <MealPage /> */}
-        <FavoritesList />
-        {/* <AddMeal /> */}
-
+        {/* Routes */}
+        <Routes>
+          <Route path="home" element={<HomePage />}/>
+          {/* Change meal page to be dynamic */}
+          {/* <Route path="mealPage/:mealID" element={<MealPage />}/> */}
+          <Route path="mealPage" element={<MealPage />}/> 
+          <Route path="addMeal" element={<AddMeal />}/>
+          <Route path="favorites" element={<FavoritesList />}/>
+        </Routes>
+        
         {/* Footer */}
         <footer>
                 <p>&copy; Julie Noh, Kyra Diaz, Tina Song, & Rishita Reddy & INFO 340</p>
